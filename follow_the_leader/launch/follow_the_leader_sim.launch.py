df1993e28337b42a9eaf72fd8e07250109eea1cb
youa <youa@oregonstate.edu>
2023-07-12 16:30:36 -0700

    updated launch files to load in YAML file for robot type

--- follow_the_leader/launch/follow_the_leader_sim.launch.py
+++ follow_the_leader/launch/follow_the_leader_sim.launch.py
@@ -3,14 +3,13 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, SetLaunchConfiguration, EmitEvent
 from launch.launch_description_sources import AnyLaunchDescriptionSource
 from launch.conditions import IfCondition, UnlessCondition, LaunchConfigurationEquals
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, PythonExpression
 from ament_index_python.packages import get_package_share_directory
 from launch_ros.actions import Node
 import os
 
 
 def generate_launch_description():
-    params_file = LaunchConfiguration('params_file')
     ur_type = LaunchConfiguration('ur_type')
     robot_ip = LaunchConfiguration('robot_ip')
     load_core = LaunchConfiguration('load_core')
@@ -18,12 +17,8 @@ def generate_launch_description():
 
     # Load the YAML file
     package_dir = get_package_share_directory('follow_the_leader')
-    params_path = os.path.join(package_dir, 'config', 'ftl_ur3.yaml')
-    params_arg = DeclareLaunchArgument(
-        'params_file',
-        default_value=params_path,
-        description='Path to the YAML file containing node parameters'
-    )
+    params_path = os.path.join(package_dir, 'config')
+    yaml_file_path = PythonExpression(["'{}/ftl_{}.yaml'.format(r'", params_path, "', '", ur_type, "')"])
 
     ur_type_arg = DeclareLaunchArgument(
         'ur_type', default_value='ur3', description='Robot description name (consistent with ur_control.launch.py)'
@@ -55,7 +50,7 @@ def generate_launch_description():
         package='follow_the_leader',
         executable='blender',
         output='screen',
-        parameters=[params_file],
+        parameters=[yaml_file_path],
         condition=IfCondition(launch_blender),
     )
 
@@ -63,13 +58,11 @@ def generate_launch_description():
         AnyLaunchDescriptionSource(
             os.path.join(get_package_share_directory('follow_the_leader'), 'launch/core_ftl_3d.launch')
         ),
-        launch_arguments=[
-            ('params_file', params_file)
-        ],
+        launch_arguments=[yaml_file_path],
         condition=IfCondition(load_core)
     )
 
     return LaunchDescription([
-        params_arg, ur_type_arg, robot_ip_arg, load_core_arg, launch_blender_arg,
+        ur_type_arg, robot_ip_arg, load_core_arg, launch_blender_arg,
         ur_launch, blender_node, core_launch
     ])
