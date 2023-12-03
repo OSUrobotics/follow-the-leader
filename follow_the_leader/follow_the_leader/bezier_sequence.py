c6cb7bc5ab21acd1d2283fd2f7b1e4b4c5c3024e
lukestroh <luke.strohbehn@gmail.com>
2023-12-03 15:59:46 -0800

    to work at home

---
+++ follow_the_leader/follow_the_leader/bezier_sequence.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+
+class BezierSequence():
+    def __init__(self) -> None:
+        
+        
+        return
+    
+
+
+def main():
+
+    return
+
+if __name__ == "__main__":
+    main()
