6b29d706504c5569ab94bb7ec149f1a0c9231b34
lukestroh <luke.strohbehn@gmail.com>
2023-12-12 12:14:29 -0800

    tree branch initial

--- follow_the_leader/follow_the_leader/tree.py
+++ follow_the_leader/follow_the_leader/tree.py
@@ -7,12 +7,28 @@ from branch import Branch
 
 from typing import List
 
+
 class Tree(Node):
     def __init__(self) -> None:
         super().__init__(name="tree_node")
 
-        self.tree: List[Branch]
+        self.tree: Branch = None
+        self.score: float = 0.0
+        self.size: float = 0.0  # (sum of the total length covered to calculate score)
+        return
+
+    def calculate_score(self):
+        return
 
     def project_onto_image(self, image):
 
         return
+
+
+def main():
+
+    return
+
+
+if __name__ == "__main__":
+    main()
